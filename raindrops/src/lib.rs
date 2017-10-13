4aabcd23e4de1c8c8a3d101f8b77ae4576f9d20b
Raj Kiran Gade <rajkiran.gade@gmail.com>
2017-10-14 01:24:02 +0530

    modified

--- raindrops/src/lib.rs
+++ raindrops/src/lib.rs
@@ -2,20 +2,15 @@ use std::string::ToString;
 
 pub fn raindrops(n: usize) -> String {
     let mut result: String = String::from("") ;
-    let mut rem: usize;
-    let n_string: String = n.to_string();
+    if n % 3 == 0 {
+        result.push_str("Pling");
+    }
+    if n % 5 == 0 {
+        result.push_str("Plang");
+    }
+    if n % 7 == 0 {
+        result.push_str("Plong");
+    }
 
-    for divisor in 1..n+1 {
-        rem = n % divisor;
-        if divisor == 3 && rem == 0 {result.push_str("Pling"); }
-        else if divisor == 5 && rem == 0 { result.push_str("Plang"); }
-        else if divisor == 7 && rem == 0 { result.push_str("Plong"); }
-    }
-    if result == "" {
-         result.push_str(&n_string);
-         result
-    }
-    else {
-        result
-    }
+    if result.len() > 0 { result} else { n.to_string()}
 }
